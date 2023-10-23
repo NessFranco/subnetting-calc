bf123e6abe5778bcc2e6416cd33ccb160b4d5436
NessFranco <nessim0xd@gmail.com>
2023-10-23 17:38:49 -0500

    Verification of input 01

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 from decoder import decoder
+import os
+
 
 def main():
+
+    #We ask for the necessary data
     ip = str(input("Inserte la ip con sufijo que desea subnetear :P\n>>>"))
-    numHosts = str(input("Inserte la cantidad de hosts que desea por subnet ;P\n>>>"))
-    mainstring = "\tIP -------> " +  ip + "\n\n\tHOST -----> " + numHosts
+    subnets = str(input("Ingrese la cantidad de subredes que necesita :P\n>>>"))
+    num_hots = str(input("Inserte la cantidad de hosts que desea por subnet ;P\n>>>"))
+
+    #We process the strings to get more refined and segmented data to use later
     octs = ip.split(".", 4)
     loct = octs[3].split("/",1)
     sfx = loct[1]
     octs[3] = loct[0]
-    ##D = decoder(mainstring)
-    ##print(D)
+    octs = list(map(int,octs))
+
+    #We validate the inputs from the user
+    for i in range(0,3):
+        if octs[i] > 255:
+            print("El octeto ", {i+1}, " es inválido")
+        
+
+    #We show back the data back for the user to verify
     print("_"*100,"\n\n")
-    print(mainstring)
+    print("\tIP -------> " +  ip + "\n\n\tHOST -----> " + num_hots + "\n\n\tSUBNETS --> " + subnets)
     print("\n\tOctetos -->",octs)
     print("\n\tSufijo --->",sfx,"\n\n")
     print("_"*100)
 
+    
+
 if __name__ == '__main__':
+    os.system('cls') #Line to clean the terminal :) 
     main()
 
